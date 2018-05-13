0b4894e073ea40f9f6c9e7d4d682a421613ff2ab
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2018-05-13 22:08:01 +0200

    Mock a typed express Request (#15)

--- src/test/routes/auth.test.ts
+++ src/test/routes/auth.test.ts
@@ -1,12 +1,16 @@
+import * as MockExpressRequest from 'mock-express-request'
+import { Request } from 'express'
+
 import { getTokenFromHeader } from '../../routes/auth'
 
 describe('Auth', () => {
   it('parses token from requests', () => {
-    const req = {
+    const req: Request = new MockExpressRequest({
       headers: {
         authorization: 'Token abcd'
       }
-    }
+    })
+
     const actual = getTokenFromHeader(req)
     const expected = 'abcd'
     expect(actual).toBe(expected)
